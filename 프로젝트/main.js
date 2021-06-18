2746aad09e0820b4421158cd9ff03bef4f939e13
butdory <butdory04@gmail.com>
2021-06-19 04:17:28 +0900

    modify main.js

--- 프로젝트/main.js
+++ 프로젝트/main.js
@@ -1,3 +1,3 @@
 function gotomain(){
-    location.replace('https:/butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html');
+    location.replace('https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html');
 }
