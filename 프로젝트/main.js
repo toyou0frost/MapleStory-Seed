65dfabfb70cc166cffa5813eaffc5488586a1972
butdory <butdory04@gmail.com>
2021-06-19 04:24:43 +0900

    modify main.js

--- 프로젝트/main.js
+++ 프로젝트/main.js
@@ -1,4 +1,5 @@
 function gotomain(){
     //location.replace('https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html');
-    window.location.replace("https://naver.com");
+    window.location.replace("https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html");
+    //window.location.href = "https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html";
 }
