0253ab2e37fbd7062c3110a14a604ec48c199343
butdory <butdory04@gmail.com>
2021-06-19 23:21:38 +0900

    전체적인 기반 완성

--- 프로젝트/main.js
+++ 프로젝트/main.js
@@ -1,5 +1,10 @@
 function gotomain(){
     //location.replace('https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html');
-    window.location.replace("https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html");
-    //window.location.href = "https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html";
+    //window.location.replace("https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html");
+    window.location.href = "https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/main.html";
+}
+
+function gotoInputF(input){
+    let url = "http://127.0.0.1:5500/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/Maple_seed/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/"+input;
+    window.location.replace(url)
 }
