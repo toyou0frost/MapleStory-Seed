6afc72545384b28972c0c9608441e499ca669091
butdory <butdory04@gmail.com>
2021-06-19 23:33:55 +0900

    modify

--- 프로젝트/main.js
+++ 프로젝트/main.js
@@ -5,6 +5,6 @@ function gotomain(){
 }
 
 function gotoInputF(input){
-    let url = "http://127.0.0.1:5500/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/Maple_seed/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/"+input;
+    let url = "https://butdory.github.io/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/Maple_seed/MapleStory-SEED/%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/"+input;
     window.location.replace(url)
 }
